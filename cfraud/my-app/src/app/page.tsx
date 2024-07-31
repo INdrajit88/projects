695b988deb57a0c7515351da1f7c9ebc2e6bb0b5
Indrajit <indrajit.ari.440@gmail.com>
2024-07-31 13:46:20 +0530

    mongo connect

--- cfraud/my-app/src/app/page.tsx
+++ cfraud/my-app/src/app/page.tsx
@@ -1,12 +1,13 @@
-import Image from "next/image";
-import { currentUser,useuser } from "@clerk/nextjs/server";
+import { currentUser, useUser } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default async function Home() {
-  const user=await currentUser();
-  
+  // const { isLoaded, isSignedIn, user } = useUser();
+  const user = await currentUser();
+  console.log(user);
   return (
-    <div className="flex justify-center items-center h-screen">
-      Homepage
+    <div className="absolute top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div className="flex flex-col items-center text-center">Home Page</div>
     </div>
-  )
+  );
 }
